f22b045eb60be4229f089ae2901696ea4ab6c413
Yash Bagri <ybagri@gmail.com>
2020-01-30 23:53:00 -0500

    finished

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const getParty = (request, response) => {
-  const file = path.resolve(__dirname, '../client/party.mp4');
+const load = (request, response, filename, mediaType) => {
+  const file = path.resolve(__dirname, filename);
 
   fs.stat(file, (err, stats) => {
     if (err) {
@@ -11,7 +11,6 @@ const getParty = (request, response) => {
       }
       return response.end(err);
     }
-
     let { range } = request.headers;
 
     if (!range) {
@@ -32,20 +31,41 @@ const getParty = (request, response) => {
     const chunksize = (end - start) + 1;
 
     response.writeHead(206, {
-      'Content-Range': `bytes ${start}-${end}/$${total}`,
+      'Content-Range': `bytes ${start}-${end}/${total}`,
       'Accept-Ranges': 'bytes',
       'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
+      'Content-Type': mediaType,
     });
 
     const stream = fs.createReadStream(file, { start, end });
 
-    stream.on('open', () => { stream.pipe(response); });
-    stream.on('error', (streamErr) => { response.end(streamErr); });
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
     return stream;
   });
 };
 
+
+const getParty = (request, response) => {
+  load(request, response, '../client/party.mp4', 'video/mp4');
+};
+
+const getBling = (request, response) => {
+  load(request, response, '../client/bling.mp3', 'audio/mpeg');
+};
+
+const getBird = (request, response) => {
+  load(request, response, '../client/bird.mp4', 'video/mp4');
+};
+
 module.exports = {
   getParty,
+  getBling,
+  getBird,
 };
